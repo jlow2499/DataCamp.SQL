0d51b74a2c450f63f6f755986647ec4a665bc01c
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2017-10-17 19:50:04 -0400

    Update master.sql

--- master.sql
+++ master.sql
@@ -1,3 +1,16 @@
+--joins
+
+SELECT c.code, c.name, c.region, e.year, p.fertility_rate,e.unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e
+ON c.code = e.code;
+
+SELECT c.name AS country, l.name AS language
+FROM countries AS c
+INNER JOIN languages AS l;
+
 SELECT c.name AS country, c.continent, l.name AS language, l.official
 FROM countries AS c
 INNER JOIN languages AS l
