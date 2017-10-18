fce483272272e6edefd5268acd3d5f9df710a5c4
Jeremiah Lowhorn <jlowhorn39@yahoo.com>
2017-10-18 11:58:52 -0400

    Update Outer_Joins.sql

--- Outer_Joins.sql
+++ Outer_Joins.sql
@@ -14,3 +14,79 @@ ON c.code = l.code
 -- sort by descending country name
 ORDER BY country DESC;
 
+-- select name, region, and gdp_percapita
+SELECT c.region, AVG(e.gdp_percapita) AS avg_gdp
+-- countries (alias c) on the left
+FROM countries AS c
+-- join with economies (alias e)
+LEFT JOIN economies AS e
+-- match on code fields
+ON c.code = e.code
+-- focus on 2010 entries
+WHERE e.year = 2010
+GROUP BY region
+ORDER BY avg_gdp DESC;
+
+/*
+SELECT cities.name AS city, urbanarea_pop, countries.name AS country,
+       indep_year, languages.name AS language, percent
+FROM cities
+LEFT JOIN countries
+ON countries.code = languages.code
+LEFT JOIN languages
+ON countries.code = languages.code
+ORDER BY city, language;
+*/
+
+SELECT cities.name AS city, urbanarea_pop, countries.name AS country,
+       indep_year, languages.name AS language, percent
+FROM languages
+RIGHT JOIN countries
+ON languages.code = countries.code
+RIGHT JOIN cities
+ON countries.code = cities.country_code
+ORDER BY city, language;
+
+--full join
+SELECT name AS country, code, region, basic_unit
+FROM countries
+FULL JOIN currencies
+USING (code)
+WHERE region = 'North America' OR region IS NULL
+ORDER BY region;
+
+--full join 2
+SELECT countries.name, code, languages.name AS language
+FROM languages
+FULL JOIN countries
+USING (code)
+WHERE countries.name LIKE 'V%' OR countries.name IS NULL
+ORDER BY countries.name;
+
+--full join 3
+SELECT c1.name AS country, region, l.name AS language,
+       basic_unit, frac_unit
+FROM countries AS c1
+FULL JOIN languages AS l
+USING (code)
+FULL JOIN currencies AS c2
+USING (code)
+WHERE region LIKE 'M%esia';
+
+---cross joins, all possible combinations
+
+SELECT c.name AS city, l.name AS language
+FROM cities AS c
+CROSS JOIN languages AS l
+WHERE c.name LIKE 'Hyder%';
+
+--outer challenge
+SELECT c.name AS country, region, p.life_expectancy AS life_exp
+FROM countries AS c
+LEFT JOIN populations AS P
+ON c.code = p.country_code
+WHERE p.year = 2010
+ORDER BY life_exp
+LIMIT 5;
+
+
